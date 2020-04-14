eef2a90451244df507394096579f8750a3f6ace9
samuelsormin <christlegacies@gmail.com>
2020-04-15 04:24:16 +0700

    add overview page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./styles/main.scss";
 import "./assets/themify-icons/themify-icons.css"
 
 import Header from "./components/layout/Header";
+import Overview from "./pages/Overview";
 
 class App extends Component {
   render() {
     return (
       <>
         <Header />
+        <Overview />
       </>
     )
   }
---
+++ src/pages/Overview.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+const Overview = () => {
+  return (
+    <section className="container-fluid">
+      <h1>Companies</h1>
+      <br/>
+      <div className="row">
+        <div className="card">
+          <div className="card-title">
+            <h2>Google</h2>
+            <span className="ti-close"></span>
+          </div>
+          <div className="card-content">
+            <div className="item">
+              <p><strong>Address:</strong></p>
+              <p className="text-grey">1600 Amphitheatre Parkway Mountain View, CA 94043 USA</p>
+            </div>
+            <div className="item">
+              <p><strong>Revenue:</strong></p>
+              <p className="text-grey">US$ 999999</p>
+            </div>
+            <div className="item">
+              <p><strong>Phone No:</strong></p>
+              <p className="text-grey">(190) 123456789</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-title">
+            <h2>Amazon</h2>
+            <span className="ti-close"></span>
+          </div>
+          <div className="card-content">
+            <div className="item">
+              <p><strong>Address:</strong></p>
+              <p className="text-grey">1600 Amphitheatre Parkway Mountain View, CA 94043 USA</p>
+            </div>
+            <div className="item">
+              <p><strong>Revenue:</strong></p>
+              <p className="text-grey">US$ 999999</p>
+            </div>
+            <div className="item">
+              <p><strong>Phone No:</strong></p>
+              <p className="text-grey">(190) 123456789</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-title">
+            <h2>Facebook</h2>
+            <span className="ti-close"></span>
+          </div>
+          <div className="card-content">
+            <div className="item">
+              <p><strong>Address:</strong></p>
+              <p className="text-grey">1600 Amphitheatre Parkway Mountain View, CA 94043 USA</p>
+            </div>
+            <div className="item">
+              <p><strong>Revenue:</strong></p>
+              <p className="text-grey">US$ 999999</p>
+            </div>
+            <div className="item">
+              <p><strong>Phone No:</strong></p>
+              <p className="text-grey">(190) 123456789</p>
+            </div>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-title">
+            <h2>Alibaba</h2>
+            <span className="ti-close"></span>
+          </div>
+          <div className="card-content">
+            <div className="item">
+              <p><strong>Address:</strong></p>
+              <p className="text-grey">1600 Amphitheatre Parkway Mountain View, CA 94043 USA</p>
+            </div>
+            <div className="item">
+              <p><strong>Revenue:</strong></p>
+              <p className="text-grey">US$ 999999</p>
+            </div>
+            <div className="item">
+              <p><strong>Phone No:</strong></p>
+              <p className="text-grey">(190) 123456789</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Overview;
